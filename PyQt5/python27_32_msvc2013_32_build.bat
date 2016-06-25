533fc62d61562a32abc6e81b6d6e548d87ab7d46
mrexodia <mr.exodia.tpodt@gmail.com>
2016-06-25 23:20:33 +0200

    disabled QtNfc (fix link errors)

--- PyQt5/python27_32_msvc2013_32_build.bat
+++ PyQt5/python27_32_msvc2013_32_build.bat
@@ -1,5 +1,5 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat"
 set QMAKESPEC=C:\Qt\qt-5.6.0-x86-msvc2013\5.6\msvc2013\mkspecs\win32-msvc2013
-C:\Python27\python.exe configure.py --verbose --confirm-license --spec=win32-msvc2013 --qmake=C:\Qt\qt-5.6.0-x86-msvc2013\5.6\msvc2013\bin\qmake.exe -a --qsci-api --target-py-version=2.7
-rem nmake
-rem nmake install
+C:\Python27\python.exe configure.py --verbose --confirm-license --spec=win32-msvc2013 --disable=QtNfc --qmake=C:\Qt\qt-5.6.0-x86-msvc2013\5.6\msvc2013\bin\qmake.exe -a --qsci-api --target-py-version=2.7
+nmake
+nmake install
