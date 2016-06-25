05f4bc16a33058b9a85a2f1b63c10f79ef901559
mrexodia <mr.exodia.tpodt@gmail.com>
2016-06-25 21:28:32 +0200

    specify QMAKESPEC in PyQt5

--- PyQt5/python27_32_msvc2013_32_build.bat
+++ PyQt5/python27_32_msvc2013_32_build.bat
@@ -1,4 +1,5 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat"
+set QMAKESPEC=C:\Qt\qt-5.6.0-x86-msvc2013\5.6\msvc2013\mkspecs\win32-msvc2013
 C:\Python27\python.exe configure.py --verbose --confirm-license --qmake=C:\Qt\qt-5.6.0-x86-msvc2013\5.6\msvc2013\bin\qmake.exe -a --qsci-api 
 nmake
 nmake install
