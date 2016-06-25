0f2dc35ad5be0a2ae706833794f7f7b325a64e8b
mrexodia <mr.exodia.tpodt@gmail.com>
2016-06-26 01:13:16 +0200

    test script

---
+++ test/test.py
@@ -0,0 +1,9 @@
+import PyQt5
+from PyQt5 import QtCore
+from PyQt5 import QtGui
+from PyQt5 import Qsci
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication
+
+if __name__ == '__main__':
+    print 'Everything appears to be working!'
